f6c86d9cfb8cd480208d8ee52f1b3983ede0d967
scottanderson <scott@getcodesmart.com>
2016-10-02 18:24:20 -0400

    Added test data from input file processes.in. Now reading each line of the input file.

--- src/com/getcodesmart/scheduler.java
+++ src/com/getcodesmart/scheduler.java
@@ -5,9 +5,37 @@
  */
 package com.getcodesmart;
 
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 public class scheduler {
 
-    public static void main(String[] args) {
+    private process[] processes;
+    private String currentDirectory = System.getProperty("user.dir") + "/src/com/getcodesmart/";
+    private String fileName;
+    private String filePath;
 
+    public static void main(String[] args) {
+        scheduler scheduler = new scheduler();
+        scheduler.initialize();
+    }
+
+    public void initialize(){
+        fileName = "processes.in";
+        filePath = currentDirectory + fileName;
+        processInputFile(filePath);
+
+    }
+
+    private static void processInputFile(String filePath){
+        try {
+            for (String line : Files.readAllLines(Paths.get(filePath))) {
+                System.out.println(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
