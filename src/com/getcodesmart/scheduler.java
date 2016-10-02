e5cb59ac9b90dda81ff855222bab9ef111e2d1ee
scottanderson <scott@getcodesmart.com>
2016-10-02 19:00:35 -0400

    Evaluation of input file and load all appropriate values.

--- src/com/getcodesmart/scheduler.java
+++ src/com/getcodesmart/scheduler.java
@@ -9,13 +9,22 @@ package com.getcodesmart;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 
 public class scheduler {
 
-    private process[] processes;
+    //Variables for reading in the input data
     private String currentDirectory = System.getProperty("user.dir") + "/src/com/getcodesmart/";
     private String fileName;
     private String filePath;
+
+    //Variables for the schedulers execution
+    List<process> processes = new ArrayList<>();
+    private int processCount;
+    private int timeBlock;
+    private String schedulerType;
+    private int timeQuantum;
 
     public static void main(String[] args) {
         scheduler scheduler = new scheduler();
@@ -23,19 +32,120 @@ public class scheduler {
     }
 
     public void initialize(){
-        fileName = "processes.in";
-        filePath = currentDirectory + fileName;
-        processInputFile(filePath);
+        setFileName("processes.in");
+        setFilePath(getCurrentDirectory() + getFileName());
+        processInputFile(getFilePath());
+
 
     }
 
-    private static void processInputFile(String filePath){
+    private void processInputFile(String filePath){
         try {
             for (String line : Files.readAllLines(Paths.get(filePath))) {
-                System.out.println(line);
+                line = line.split("#")[0];
+                evaluateFileLine(line);
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    private void evaluateFileLine(String line){
+        String command = line.split(" ")[0];
+        int parameterValue;
+
+        switch(command){
+            case "processcount":
+                System.out.println("This is the processcount");
+                parameterValue = Integer.parseInt(line.split(" ")[1]);
+                setProcessCount(parameterValue);
+                break;
+            case "runfor":
+                System.out.println("This is the timeblock");
+                parameterValue = Integer.parseInt(line.split(" ")[1]);
+                setTimeBlock(parameterValue);
+                break;
+            case "use":
+                System.out.println("This is the command");
+                String commandValue = line.split(" ")[1];
+                setSchedulerType(commandValue);
+                break;
+            case "quantum":
+                System.out.println("This is the time quantum");
+                parameterValue = Integer.parseInt(line.split(" ")[1]);
+                setTimeQuantum(parameterValue);
+                break;
+            case "process":
+                System.out.println("This is a process");
+                String processName = line.split(" ")[2];
+                int processArrivalTime = Integer.parseInt(line.split(" ")[4]);
+                int processBurstTime = Integer.parseInt(line.split(" ")[6]);
+                addProcess(new process(processName,processArrivalTime,processBurstTime));
+
+                break;
+            case "end":
+                System.out.println("This is the end of the file");
+                break;
+        }
+    }
+
+    public void addProcess(process newProcess){
+        processes.add(newProcess);
+    }
+
+    public String getCurrentDirectory() {
+        return currentDirectory;
+    }
+
+    public void setCurrentDirectory(String currentDirectory) {
+        this.currentDirectory = currentDirectory;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public String getFilePath() {
+        return filePath;
+    }
+
+    public void setFilePath(String filePath) {
+        this.filePath = filePath;
+    }
+
+    public int getProcessCount() {
+        return processCount;
+    }
+
+    public void setProcessCount(int processCount) {
+        this.processCount = processCount;
+    }
+
+    public int getTimeBlock() {
+        return timeBlock;
+    }
+
+    public void setTimeBlock(int timeBlock) {
+        this.timeBlock = timeBlock;
+    }
+
+    public String getSchedulerType() {
+        return schedulerType;
+    }
+
+    public void setSchedulerType(String schedulerType) {
+        this.schedulerType = schedulerType;
+    }
+
+    public int getTimeQuantum() {
+        return timeQuantum;
+    }
+
+    public void setTimeQuantum(int timeQuantum) {
+        this.timeQuantum = timeQuantum;
     }
 }
