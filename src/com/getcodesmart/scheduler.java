583ca8421d84306cad5472aeedea91e21fe714b9
scottanderson <scott@getcodesmart.com>
2016-10-02 17:47:24 -0400

    Added process object class.

--- src/com/getcodesmart/scheduler.java
+++ src/com/getcodesmart/scheduler.java
@@ -1,9 +1,13 @@
+/*
+    Group Members: Scott Anderson, Travis Le, and Luis Velez
+    COP 4600 Assignment 1 Schedulers
+    October 2, 2016
+ */
 package com.getcodesmart;
 
 public class scheduler {
 
     public static void main(String[] args) {
-	// write your code here
-        System.out.println("Testing");
+
     }
 }
